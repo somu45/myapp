160239f75b8d9f8a094ac01deaf5bc2af7d19fc6
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2016-01-11 04:46:51 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module Myapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.js_compressor = :uglify
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.precompile += %w(application.css application.js)
   end
 end
